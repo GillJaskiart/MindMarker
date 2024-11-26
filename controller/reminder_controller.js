cfc3dc2ec2103b4d636656d074ef29b9cbbaae4d
Chelsie Lele <lelechelsie@gmail.com>
2024-11-26 00:07:18 -0800

    Worked on edit-reminder and delete-reminder, both are functional now

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -42,11 +42,41 @@ let remindersController = {
 
   update: (req, res) => {
     // implement this code
+    // console.log(req.params)
+    // console.log(req.body)
+
+    // getting the reminder's id
+    const reminderId = parseInt(req.params.id)
+
+    const reminderToEdit = database.cindy.reminders.find( (reminder) => reminder.id === reminderId)
+    if (reminderToEdit) {
+      reminderToEdit.title = req.body.title
+      reminderToEdit.description = req.body.description
+      reminderToEdit.completed = req.body.completed === "true" ? true : false
+    } else {
+      console.log(`Reminder with ID ${reminderId} not found`);
+  }
+    res.redirect("/reminders");
   },
 
   delete: (req, res) => {
     // Implement this code
-  },
+    // console.log(req.params)
+    // console.log(req.body)
+    const reminderId = parseInt(req.params.id)
+
+    const reminderToDelete = database.cindy.reminders.find( (reminder) => reminder.id === reminderId)
+    if (reminderToDelete) {
+      for (let i = database.cindy.reminders.length - 1; i >= 0; i--) {
+        if (database.cindy.reminders[i] === reminderToDelete) {
+          database.cindy.reminders.splice(i, 1);
+        }
+    }
+    } else {
+      console.log(`Reminder with ID ${reminderId} not found`);
+    }
+    res.redirect("/reminders");
+  }, 
 };
 
 module.exports = remindersController;
--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const reminderController = require("./controller/reminder_controller");
 const authController = require("./controller/auth_controller");
 // const express = require("express");
 
-app.use(
-  session({
-    secret: "secret", // used to make the cookie stored in the browser digitially signed
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000,
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: "secret", // used to make the cookie stored in the browser digitially signed
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       httpOnly: true,
+//       secure: false,
+//       maxAge: 24 * 60 * 60 * 1000,
+//     },
+//   })
+// );
 
-const passport = require("./middleware/passport");
+// const passport = require("./middleware/passport");
 
-app.use(passport.initialize()); // means 'start passport'
-app.use(passport.session());
+// app.use(passport.initialize()); // means 'start passport'
+// app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -33,20 +33,26 @@ app.use(ejsLayouts);
 
 // Routes start here
 
+// case 1: show a list of reminders
 app.get("/reminders", reminderController.list);
 
+// case 2: user creates a new reminder
 app.get("/reminder/new", reminderController.new);
 
 app.get("/reminder/:id", reminderController.listOne);
 
+// case 6: Edit an individual reminder
 app.get("/reminder/:id/edit", reminderController.edit);
 
+// When the user clicks 'submit' on the create reminder page, the reminder is added to the list of reminders
 app.post("/reminder/", reminderController.create);
 
 // Implement this yourself
+// user clicks the update button from case 6 and expects their reminder to be updated.
 app.post("/reminder/update/:id", reminderController.update);
 
 // Implement this yourself
+// user clicks the delete button and we expect the reminder to be deleted
 app.post("/reminder/delete/:id", reminderController.delete);
 
 // We will fix this soon.
