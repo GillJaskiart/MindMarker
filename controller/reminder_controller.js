7fd0bb55310f91a9a03160d5ba86d1a6909d867f
GillJaskiart <jgill391@my.bcit.ca>
2024-12-02 15:29:44 -0800

    prisma database integration for reminder_controller.js done! All the reminder pages are using the new database.

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -1,85 +1,109 @@
 let database = require("../models/userModel");
 
-let remindersController = {
-  list: (req, res, user) => {
-    console.log("req.user for /reminders redirection from dashboard: ",  req.user)
-    // userID = req.user.id
-    // console.log(database.database[userID - 1].reminders)
+// Every time we use db, we are talking to the prisms database
+const { PrismaClient } = require("@prisma/client");
+const db = new PrismaClient();
 
-    res.render("reminder/index", { reminders: database.database[req.user.id - 1].reminders });
+let remindersController = {
+  list:  async (req, res, user) => {
+    const reminders = await db.reminder.findMany({
+      where: { userId: req.user.id },
+    });
+    console.log(reminders);
+      
+    res.render("reminder/index", { reminders:  reminders});
   },
 
   new: (req, res) => {
     res.render("reminder/create");
   },
 
-  listOne: (req, res) => {
+  listOne: async (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = database.database[req.user.id - 1].reminders.find(function (reminder) {
-      return reminder.id == reminderToFind;
+
+    // let searchResult = database.database[req.user.id - 1].reminders.find(function (reminder) {
+    //   return reminder.id == reminderToFind;
+    // });
+    let searchResult = await db.reminder.findUnique({
+      where: { id: parseInt(reminderToFind) },
     });
+
     if (searchResult != undefined) {
       res.render("reminder/single-reminder", { reminderItem: searchResult });
     } else {
-      res.render("reminder/index", { reminders: database.database[req.user.id - 1].reminders });
+      let reminders = await db.reminder.findMany({
+        where: { userId: req.user.id },
+      });
+      res.render("reminder/index", { reminders: reminders });
     }
   },
 
-  create: (req, res) => {
-    let reminder = {
-      id: (database.database[req.user.id - 1].reminders).length + 1,
-      title: req.body.title,
-      description: req.body.description,
-      completed: false,
-    };
-    database.database[req.user.id - 1].reminders.push(reminder);
+  create: async (req, res) => {
+    const user = req.user.id;
+    await db.reminder.create({
+      data:{
+        title: req.body.title,
+        description: req.body.description,
+        completed: false,
+        user: {connect: {id: req.user.id}}
+      }
+    });
     res.redirect("/reminders");
   },
 
-  edit: (req, res) => {
+  edit: async (req, res) => {
     let reminderToFind = req.params.id;
-    let searchResult = database.database[req.user.id - 1].reminders.find(function (reminder) {
-      return reminder.id == reminderToFind;
+    let searchResult = await db.reminder.findUnique({
+      where: { id: parseInt(reminderToFind) },
     });
     res.render("reminder/edit", { reminderItem: searchResult });
   },
 
-  update: (req, res) => {
-    // implement this code
-    // console.log(req.params)
-    // console.log(req.body)
-
-    // getting the reminder's id
+  update: async (req, res) => {
     const reminderId = parseInt(req.params.id)
 
-    const reminderToEdit = database.database[req.user.id - 1].reminders.find( (reminder) => reminder.id === reminderId)
+    const reminderToEdit = await db.reminder.findUnique({
+      where: { id: parseInt(reminderId) },
+    });
+
     if (reminderToEdit) {
-      reminderToEdit.title = req.body.title
-      reminderToEdit.description = req.body.description
-      reminderToEdit.completed = req.body.completed === "true" ? true : false
-    } else {
+      await db.reminder.update({
+        where: { id: parseInt(reminderId) },
+        data: {
+          title: req.body.title,
+          description: req.body.description,
+          completed: req.body.completed === "true" ? true : false,
+        },
+      });
+    }
+    else {
       console.log(`Reminder with ID ${reminderId} not found`);
-  }
+    }
     res.redirect("/reminders");
   },
 
-  delete: (req, res) => {
-    // Implement this code
-    // console.log(req.params)
-    // console.log(req.body)
+  delete: async (req, res) => {
     const reminderId = parseInt(req.params.id)
-
-    const reminderToDelete = database.database[req.user.id - 1].reminders.find( (reminder) => reminder.id === reminderId)
-    if (reminderToDelete) {
-      for (let i = database.database[req.user.id - 1].reminders.length - 1; i >= 0; i--) {
-        if (database.database[req.user.id - 1].reminders[i] === reminderToDelete) {
-          database.database[req.user.id - 1].reminders.splice(i, 1);
-        }
-    }
-    } else {
+    
+    if (reminderId == undefined) {
       console.log(`Reminder with ID ${reminderId} not found`);
+    } else {
+    await db.reminder.delete({
+      where: { id: parseInt(reminderId) },
+    });
     }
+
     res.redirect("/reminders");
+    
+    // if (reminderToDelete) {
+    //   for (let i = database.database[req.user.id - 1].reminders.length - 1; i >= 0; i--) {
+    //     if (database.database[req.user.id - 1].reminders[i] === reminderToDelete) {
+    //       database.database[req.user.id - 1].reminders.splice(i, 1);
+    //     }
+    // }
+    // } else {
+    //   console.log(`Reminder with ID ${reminderId} not found`);
+    // }
   }, 
 };
 
